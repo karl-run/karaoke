b0d6f3c6f334734d0f3a6a79cb6cac72ecc0b24f
Karl O <k@rl.run>
2024-03-30 12:48:08 +0100

    feat: mobile first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import { search } from "@/spotify/search";
 import PlaySong from "@/components/PlaySong";
 import SearchBar from "@/components/SearchBar";
-import React, { Suspense } from "react";
+import React, { PropsWithChildren, Suspense } from "react";
 import UserBar from "@/components/rsc/UserBar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface Props {
   searchParams: {
@@ -13,35 +14,56 @@ interface Props {
 
 export default async function Home({ searchParams }: Props) {
   return (
-    <main className="">
+    <div className="container">
       <UserBar>
         <SearchBar />
       </UserBar>
       <Suspense key={searchParams.q} fallback={<TrackSearchSkeleton />}>
         <TrackSearch query={searchParams.q} />
       </Suspense>
-    </main>
+    </div>
   );
 }
 
 async function TrackSearch({ query }: { query: string }) {
+  if (!query)
+    return (
+      <div className="p-20 flex justify-center items-center">
+        <div className="text-xl opacity-70">Start searching for bangers</div>
+      </div>
+    );
+
   const result = await search(query, "track");
 
+  if (result.length === 0) {
+    return (
+      <div className="p-20 flex justify-center items-center">
+        <div className="text-xl opacity-70">
+          No songs found matching &quot;{query}&quot;
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="grid grid-cols-6 gap-2 p-2">
+    <TrackSearchGrid>
       {result.map((track) => (
-        <div key={track.id} className="flex flex-col">
+        <div key={track.id} className="flex flex-row xs:flex-col">
           <Image
-            className="col-span-2"
+            className="h-16 w-16 xs:w-full xs:h-auto aspect-square rounded-xl"
             src={track.image.url}
             alt={track.name}
             width={200}
             height={200}
           />
-          <div className="flex items-center p-1">
-            <div className="grow overflow-hidden text-sm">
-              <div>{track.name}</div>
-              <div>{track.artist}</div>
+          <div className="flex items-center p-1 grow">
+            <div className="grow text-sm">
+              <div className="max-w-[200px] truncate" title={track.name}>
+                {track.name}
+              </div>
+              <div className="truncate" title={track.artist}>
+                {track.artist}
+              </div>
             </div>
             {track.preview_url && (
               <div className="h-10 w-10">
@@ -51,20 +73,33 @@ async function TrackSearch({ query }: { query: string }) {
           </div>
         </div>
       ))}
-    </div>
+    </TrackSearchGrid>
   );
 }
 
 function TrackSearchSkeleton() {
   return (
-    <div className="grid grid-cols-8">
-      {[...Array(8)].map((_, index) => (
-        <div key={index} className="col-span-2">
-          <div className="bg-gray-200 w-40 h-40"></div>
-          <div className="bg-gray-200 w-40 h-4"></div>
-          <div className="bg-gray-200 w-40 h-4"></div>
+    <TrackSearchGrid>
+      {[...Array(20)].map((_, index) => (
+        <div key={index} className="flex flex-col">
+          <Skeleton className="aspect-square max-w-full" />
+          <div className="flex items-center p-1 gap-1">
+            <div className="grow flex flex-col gap-1">
+              <Skeleton className="h-4" />
+              <Skeleton className="h-4" />
+            </div>
+            <Skeleton className="h-10 w-10" />
+          </div>
         </div>
       ))}
+    </TrackSearchGrid>
+  );
+}
+
+function TrackSearchGrid({ children }: PropsWithChildren) {
+  return (
+    <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6 gap-2 p-2">
+      {children}
     </div>
   );
 }
