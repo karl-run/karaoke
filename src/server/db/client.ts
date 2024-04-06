553ab5fd09045e06799cab3bfcbf0d73d04ff5af
Karl O <k@rl.run>
2024-04-06 23:29:53 +0200

    chore: test out drizzle on one query

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
 
 import { raise } from '@/utils/ts';
 
@@ -6,3 +7,7 @@ export const client = createClient({
   url: process.env.TURSO_DB_URL ?? raise('TURSO_DB_URL is required'),
   authToken: process.env.TURSO_DB_TOKEN ?? raise('TURSO_DB_TOKEN is required'),
 });
+
+export const db = drizzle(client);
+
+export * from './tables';
--- src/server/db/stats.ts
+++ src/server/db/stats.ts
@@ -1,4 +1,6 @@
-import { client } from 'server/db/client';
+import { count } from 'drizzle-orm';
+
+import { client, db, users } from 'server/db/client';
 
 export async function getTotalBangersCount() {
   const result = await client.execute('SELECT COUNT(*) FROM bangers');
@@ -8,15 +10,14 @@ export async function getTotalBangersCount() {
 }
 
 export async function getTotalUserCount() {
-  const result = await client.execute('SELECT COUNT(*) FROM users');
-  const count = result.rows[0][0];
+  const result = await db.select({ count: count() }).from(users);
 
-  return count as number
+  return result[0].count;
 }
 
 export async function getTotalGroupCount() {
   const result = await client.execute('SELECT COUNT(*) FROM user_group');
   const count = result.rows[0][0];
 
-  return count as number
+  return count as number;
 }
