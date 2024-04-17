81e2c5ead63702d4f22b8fc6948fabda66b4b039
Karl J. Overå <k@rl.run>
2024-04-17 14:35:23 +0200

    fix: remove weird empty tracks in playlists

--- src/server/spotify/playlist.ts
+++ src/server/spotify/playlist.ts
@@ -12,7 +12,13 @@ export async function getTracksInPlaylist(playlistId: string) {
   try {
     const playlist = await spotifyFetch<PlaylistResponse>(`/v1/playlists/${playlistId}`);
 
-    return playlist.tracks.items.map((item) => item.track).map(spotifyTrackToTrackResult);
+    return (
+      playlist.tracks.items
+        .map((item) => item.track)
+        .map(spotifyTrackToTrackResult)
+        // Remove weird empty track?!
+        .filter((it) => it.name !== '')
+    );
   } catch (e) {
     return {
       errorMessage: (e as Error).message,
