072677362e257e2a2e12155c71943f5a639e6473
Karl O <k@rl.run>
2024-04-18 23:10:33 +0200

    fix: also save to normalized cache when saving

--- src/server/bangers/bangers-cache.ts
+++ src/server/bangers/bangers-cache.ts
@@ -1,11 +1,19 @@
 import { TrackResult } from 'server/spotify/types';
-import { db, songCache } from 'server/db';
+import { db, normalizedSongCache, songCache } from 'server/db';
+import { trackToNormalizedId } from 'server/bangers/normalization';
 
 export async function addToCache(song: TrackResult) {
   await db
     .insert(songCache)
     .values({
       songId: song.id,
+      data: song,
+    })
+    .onConflictDoNothing();
+  await db
+    .insert(normalizedSongCache)
+    .values({
+      songKey: trackToNormalizedId(song),
       data: song,
     })
     .onConflictDoNothing();
