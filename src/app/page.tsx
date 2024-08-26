675600661b0fd27bbaad4925689fe47a675f0aca
Karl J. Overå <k@rl.run>
2024-08-26 18:43:08 +0200

    style: make explore feature more prominent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import Link from 'next/link'
+import { GalleryHorizontalEnd } from 'lucide-react'
 
 import { searchTracks } from 'server/spotify/track'
 import { TrackResult } from 'server/spotify/types'
@@ -11,6 +12,7 @@ import Track, { TrackSkeleton } from '@/components/track/Track'
 import { TrackGrid } from '@/components/track/TrackGrid'
 import { FullPage } from '@/components/layout/Layouts'
 import { Button } from '@/components/ui/button'
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 
 interface Props {
   searchParams: {
@@ -35,20 +37,33 @@ export default async function Home({ searchParams }: Props) {
 async function TrackSearch({ query }: { query: string }) {
   if (!query || query.length < 4)
     return (
-      <div className="relative overflow-hidden h-64">
-        <TrackGrid>
-          <TrackSkeleton />
-          <TrackSkeleton />
-          <TrackSkeleton />
-          <TrackSkeleton />
-          <TrackSkeleton />
-        </TrackGrid>
-        <div className="absolute top-0 h-72 p-8 w-full bg-gradient-to-b from-transparent to-background flex items-center justify-center flex-col gap-16">
-          <div className="text-xl opacity-70 text-center">Start typing to search for new bangers...</div>
-          <Button size="lg" asChild>
-            <Link href="/explore">Or explore...</Link>
-          </Button>
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <div className="relative overflow-hidden h-64 w-full">
+          <TrackGrid>
+            <TrackSkeleton />
+            <TrackSkeleton />
+            <TrackSkeleton />
+            <TrackSkeleton />
+            <TrackSkeleton />
+          </TrackGrid>
+          <div className="absolute top-0 h-72 p-8 w-full bg-gradient-to-b from-transparent to-background flex items-center justify-center flex-col gap-16">
+            <div className="text-xl opacity-70 text-center">Start typing to search for new bangers...</div>
+          </div>
         </div>
+        <Card className="max-w-prose m-4">
+          <CardHeader>
+            <CardTitle>Or explore songs</CardTitle>
+            <CardDescription>Find songs popular amongst your friends, or songs popular globally</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex justify-center">
+            <Button size="lg" asChild className="grow-0">
+              <Link href="/explore">
+                Start exploring
+                <GalleryHorizontalEnd className="ml-2" />
+              </Link>
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     )
 
